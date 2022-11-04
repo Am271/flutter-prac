e26184de058980c505a8ee2791f727959ebcf7c7
Amitash M S <amitu895@gmail.com>
2022-11-04 23:39:13 +0530

    Container and scaffold used

--- lib/main.dart
+++ lib/main.dart
@@ -3,136 +3,62 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
       ),
-    );
-  }
+    ),
+  );
+}
 
+class SimpStuff extends StatefulWidget {
+  const SimpStuff({super.key});
+
+  @override
+  State<SimpStuff> createState() => _SimpStuffState();
+}
+
+class _SimpStuffState extends State<SimpStuff> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // NEW from here ...
       appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu, color: Colors.white,),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
-          ),
+        leading: const Icon(Icons.headphones, color: Colors.amber,),
+        title: const Text('How are you'),
+        actions: const [
+          Icon(Icons.navigate_next),
         ],
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider(); /*2*/
+      body: const Text('This is crazy man'),
+    );
+  }
+}
 
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
 
-          final alreadySaved = _saved.contains(_suggestions[index]);
-
-          return ListTile(
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont,
-            ),
-            trailing: IconButton(
-              // NEW from here ...
-              // alreadySaved ? Icons.favorite : Icons.favorite_border,
-              icon: alreadySaved
-                  ? const Icon(Icons.favorite)
-                  : const Icon(Icons.favorite_border),
-              color: alreadySaved ? Colors.red : Colors.blue,
-              onPressed: () {
-                setState(() {
-                  if (alreadySaved) {
-                    _saved.remove(_suggestions[index]);
-                  } else {
-                    _saved.add(_suggestions[index]);
-                  }
-                });
-              },
-              // semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-            ),
-            // onTap: () {
-            //   // NEW from here ...
-            //   setState(() {
-            //     if (alreadySaved) {
-            //       _saved.remove(_suggestions[index]);
-            //     } else {
-            //       _saved.add(_suggestions[index]);
-            //     }
-            //   }); // to here.
-            // },
-          );
-        },
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        onPressed: null,
-        child: Icon(Icons.add),
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        int a = 1;
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Engage', style: TextStyle(color: Colors.white),),
+        ),
       ),
     );
   }
