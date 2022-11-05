6d7006a4298cdb393d38724236ce7ca6d38ca97a
Amitash M S <amitu895@gmail.com>
2022-11-05 18:53:31 +0530

    Showing a simple image

--- lib/main.dart
+++ lib/main.dart
@@ -3,137 +3,33 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: Map(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Map extends StatefulWidget {
+  const Map({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Welcome to Flutter', home: RandomWords());
-  }
+  State<Map> createState() => _MapState();
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-
+class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // NEW from here ...
-      appBar: AppBar(
-        leading: const IconButton(
-          icon: Icon(Icons.menu, color: Colors.white,),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions',
-          ),
-        ],
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-
-          final alreadySaved = _saved.contains(_suggestions[index]);
-
-          return ListTile(
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont,
+        appBar: AppBar(title: const Text('MapView')),
+        body: Column(
+          children: [
+            Image.asset('./assets/bird.jpg'),
+            Container(
+              height: 30,
             ),
-            trailing: IconButton(
-              // NEW from here ...
-              // alreadySaved ? Icons.favorite : Icons.favorite_border,
-              icon: alreadySaved
-                  ? const Icon(Icons.favorite)
-                  : const Icon(Icons.favorite_border),
-              color: alreadySaved ? Colors.red : Colors.blue,
-              onPressed: () {
-                setState(() {
-                  if (alreadySaved) {
-                    _saved.remove(_suggestions[index]);
-                  } else {
-                    _saved.add(_suggestions[index]);
-                  }
-                });
-              },
-              // semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-            ),
-            // onTap: () {
-            //   // NEW from here ...
-            //   setState(() {
-            //     if (alreadySaved) {
-            //       _saved.remove(_suggestions[index]);
-            //     } else {
-            //       _saved.add(_suggestions[index]);
-            //     }
-            //   }); // to here.
-            // },
-          );
-        },
-      ),
-      floatingActionButton: const FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        onPressed: null,
-        child: Icon(Icons.add),
-      ),
-    );
+            const Center(child: Text('This is an image of a bird'))
+          ],
+        ));
   }
 }
