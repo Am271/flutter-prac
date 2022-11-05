0216ba909e26ac5422ee13df381bafcc0e1810bb
Amitash M S <amitu895@gmail.com>
2022-11-05 20:57:13 +0530

    Now offset values are displayed when the map is swiped across

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,103 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Map(),
+    home: MapView(),
   ));
 }
 
-class Map extends StatefulWidget {
-  const Map({super.key});
+class MapView extends StatefulWidget {
+  const MapView({super.key});
 
   @override
-  State<Map> createState() => _MapState();
+  State<MapView> createState() => _MapViewState();
 }
 
-class _MapState extends State<Map> {
+class _MapViewState extends State<MapView> {
+  double offsetX = 0.0;
+  double offsetY = 0.0;
+
+  void _updateOffsetX(velocity) {
+    setState(() {
+      if (velocity > 0) {
+        offsetX += 20;
+      } else {
+        offsetX -= 20;
+      }
+    });
+  }
+
+  void _updateOffsetY(velocity) {
+    setState(() {
+      if (velocity > 0) {
+        offsetY += 20;
+      } else {
+        offsetY -= 20;
+      }
+    });
+  }
+
+  void _start() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (offsetX > 300) {
+          timer.cancel();
+        }
+        offsetX += 50;
+        offsetY += 50;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('MapView')),
-        body: Column(
-          children: [
-            Image.asset('./assets/bird.jpg'),
-            Container(
-              height: 30,
-            ),
-            const Center(child: Text('This is an image of a bird'))
-          ],
-        ));
+        appBar: AppBar(title: const Text('Map View')),
+        body: Stack(children: [
+          Transform.translate(
+              offset: Offset(offsetX, offsetY),
+              child: Transform.scale(
+                  scale: 4.5,
+                  child: ClipRect(
+                    child: Image.asset(
+                      'assets/bird.jpg',
+                    ),
+                  ))),
+          GestureDetector(
+              // onTap: () {
+              //   _start();
+              // },
+              onHorizontalDragEnd: ((details) {
+                _updateOffsetX(details.primaryVelocity);
+              }),
+              onVerticalDragEnd: (details) {
+                _updateOffsetY(details.primaryVelocity);
+              },
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.white),
+                height: 80,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 10,
+                    ),
+                    Text(
+                      'offset X = $offsetX',
+                    ),
+                    Container(
+                      height: 30,
+                    ),
+                    Text(
+                      'offset Y = $offsetY',
+                    )
+                  ],
+                ),
+              ))
+        ]));
   }
 }
